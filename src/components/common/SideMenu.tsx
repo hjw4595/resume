22118f4595d933c306374820d57208debe3fad05
hjw4595 <hongjung73@gmail.com>
2023-05-03 18:17:58 +0900

    전체적인 레이아웃 마무리 / 스크롤 클릭시 이동하도록 추가

--- src/components/common/SideMenu.tsx
+++ src/components/common/SideMenu.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
 const ITEM_LIST = [
-  { title: "1", herf: "/" },
-  { title: "2", herf: "/" },
-  { title: "3", herf: "/" },
-  { title: "4", herf: "/" },
+  { title: "Intro", herf: "/" },
+  { title: "Career", herf: "/" },
+  { title: "Skill", herf: "/" },
+  { title: "Other", herf: "/" },
 ];
 
 interface Menu {
@@ -15,10 +15,21 @@ interface Menu {
 const menuItem = (args: Menu) => <Link to={args.herf}>{args.title}</Link>;
 
 function SideMenu() {
+  const handleClickMenu = (id: string) => {
+    const element = document.getElementById(`${id}`);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
-    <div className="p-2">
+    <div className="sticky top-24 p-2">
+      <span>목차</span>
       {ITEM_LIST.map((item) => {
-        return <div>{menuItem(item)}</div>;
+        return (
+          <div onClick={() => handleClickMenu(item.title)}>
+            {menuItem(item)}
+          </div>
+        );
       })}
     </div>
   );
